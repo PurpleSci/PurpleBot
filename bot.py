c8296cbaa7b1510fde4bd344575deca69a0a987c
Aaron <60256394+tuxedlinux@users.noreply.github.com>
2020-09-25 19:11:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,4 +97,14 @@ async def rubbish(ctx):
 async def on_disconnect():
     print("PurpleBot disconnected.")
 
+# Let's make the Discord.py module understand that there is a folder for additional commands (cogs).
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
 client.run(os.environ['DISCORD_TOKEN'])
