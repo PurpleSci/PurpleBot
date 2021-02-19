bc0d74183f1361e84be27e5d1d98269b5f55fc34
PurpleSci <yanikyanus@gmail.com>
2021-02-19 21:51:35 +0300

    Updated the bot

--- bot.py
+++ bot.py
@@ -334,6 +334,10 @@ async def divide(ctx,a,b):
     await ctx.send(int(a) / int(b))
 
 @client.command()
+async def power(ctx,a,b):
+    await ctx.send(int(a) ** int(b))
+
+@client.command()
 async def pi(ctx):
     await ctx.send(f'π = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679...')
 
