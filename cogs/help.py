e4e7175c69892c2fa6de40cd60d33827cb2b73b6
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-05-28 22:15:39 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -13,14 +13,14 @@ class Help(commands.Cog):
         embed = discord.Embed(title=f"PurpleBot Command List",
                           color=ctx.guild.me.top_role.color, 
                           timestamp=ctx.message.created_at)
-        embed.add_field(name="Moderation", value=f"`ban, kick, mute, unban, unmute`")
-        embed.add_field(name="Fun", value=f"`crabrave, die, hello, piglatin, \
-                                             pog, predict, randnum, rick, \
-                                             sans, say`")
-        embed.add_field(name="Science & Math", value=f"`add, subtract, multiply, divide, \
-                                                        power, sqrt, pi, weather`")
-        embed.add_field(name="Utility", value=f"`about, avatar, github, help, \
-                                                 invite, license, ping`")                                                
+        embed.add_field(name="Moderation", value=f"`ban, kick, mute, unban, unmute`", inline=False)
+        embed.add_field(name="Fun", value=f"`crabrave, die, hello, piglatin,
+                                             pog, predict, randnum, rick,
+                                             sans, say`", inline=False)
+        embed.add_field(name="Science & Math", value=f"`add, subtract, multiply, divide,
+                                                        power, sqrt, pi, weather`", inline=False)
+        embed.add_field(name="Utility", value=f"about, avatar, github, help,
+                                                invite, license, ping", inline=False)                                                
         await ctx.send(embed=embed)
 
 def setup(client):
