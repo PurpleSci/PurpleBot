2f59d3aa3dcd857bd1b68e111ebbdf022b4f68af
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-05-31 22:30:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,23 +118,18 @@ async def pi(ctx):
     await ctx.send(f'π = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679...')
 
 @bot.command()
-async def rubbish(ctx, lang):
+async def rubbish(ctx, lang=en):
     sentence = ""
-    if lang == "ru":
-        for i in range(random.randrange(3,7)):
-            word = str()
-            for j in range(random.randrange(1,5)):
-                word = word + random.choice(consonants_ru) + random.choice(vowels_ru)
-            sentence = sentence + word + " "
-        await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
-    else:
-        for i in range(random.randrange(3,7)):
-            word = str()
+    for i in range(random.randrange(3,7)):
+        word = str()
+        if lang == "en":
             for j in range(random.randrange(1,5)):
                 word = word + random.choice(consonants_en) + random.choice(vowels_en)
-            sentence = sentence + word + " "
-        await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
-
+        if lang == "ru":
+            for j in range(random.randrange(1,5)):
+                word = word + random.choice(consonants_ru) + random.choice(vowels_ru)
+        sentence = sentence + word + " "
+    await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
 
 @bot.command()
 async def say(ctx,*,arg):
