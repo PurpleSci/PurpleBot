7504f01ee597be2294638e1f5a026f6a5c02fb29
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-05-31 22:32:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ async def pi(ctx):
     await ctx.send(f'π = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679...')
 
 @bot.command()
-async def rubbish(ctx, lang=en):
+async def rubbish(ctx, lang="en"):
     sentence = ""
     for i in range(random.randrange(3,7)):
         word = str()
