0459b5a968a65d1523e1c748b9f54cb8fcaaacc0
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-05-31 22:17:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -122,12 +122,12 @@ async def rubbish(ctx, lang):
     sentence = ""
     for i in range(random.randrange(3,7)):
         word = str()
-        if lang == "en":
-            for j in range(random.randrange(1,5)):
-                word = word + random.choice(consonants_en) + random.choice(vowels_en)
         if lang == "ru":
             for j in range(random.randrange(1,5)):
                 word = word + random.choice(consonants_ru) + random.choice(vowels_ru)
+        else:
+            for j in range(random.randrange(1,5)):
+                word = word + random.choice(consonants_en) + random.choice(vowels_en)
         sentence = sentence + word + " "
     await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
 
