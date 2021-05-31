3b067c66b6291914b18beaebd5a79ce9a8b04700
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-05-31 21:21:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! ({round(bot.latency * 1000)} ms)')
+    await ctx.send(f'Pong! **{round(bot.latency * 1000)} ms**')
 
 @bot.command()
 async def about(ctx):
