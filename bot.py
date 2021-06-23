70f411d33b1f6f411567057e0b6341a1257a3907
Purple Scientist <44437701+PurpleSci@users.noreply.github.com>
2021-06-23 17:41:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,8 +118,8 @@ async def pi(ctx):
     await ctx.send(f'π = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679...')
 
 @bot.command()
-async def name(ctx, arg=random.choice('male','female')):
-    names.get_full_name(gender=arg)
+async def name(ctx):
+    names.get_full_name()
 
 @bot.command()
 async def shuffle(ctx, word):
